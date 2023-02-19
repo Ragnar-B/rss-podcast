c325d707a08e5518838baf3aa4e4d41c42a940f6
Ragnar-B <mw.vandijk@gmail.com>
2023-02-19 22:13:22 +0100

    update

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import feedparser
-NewsFeed = feedparser.parse("https://feeds.fireside.fm/selfhosted/rss")
-entry = NewsFeed.entries[1]
+import os
 
-print(entry.id)
+# #Environment Variables
+# path  = './'
+# rss = feedparser.parse("https://feeds.fireside.fm/selfhosted/rss")
+
+#CLI Version
+path = input("Which directory need to be scanned? ")
+rss = feedparser.parse(input("Which feed do you want to parse? "))
+
+def main():
+    for episode in range(0, len(rss.entries) -1):
+        id = rss.entries[episode]
+        for file in os.listdir(path):
+            episode_mp3 = id.id + ".mp3"
+            if file == episode_mp3:
+                os.rename(episode_mp3, id.title + ".mp3")
+                print(f"renamed {id.id} to {id.title}")
+
+if __name__ == "__main__":
+    main()
